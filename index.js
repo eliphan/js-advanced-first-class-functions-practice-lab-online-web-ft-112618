a491d8008ca328487a1a3cae2462399a16b7e608
Elise Phan <aliceconnic@gmail.com>
2019-03-14 23:29:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,7 @@ const totalRevenue = function (drivers) {
     return currentDriver.revenue + total;
   }, 0);
 };
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
