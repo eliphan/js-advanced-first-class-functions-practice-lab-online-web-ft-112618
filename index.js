245da24a884098cc87c26b3d5de4344b77d8b03d
Elise Phan <aliceconnic@gmail.com>
2019-03-14 22:52:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const logDriversByHometown = function (drivers, location) {
 
 const driversByRevenue = function (drivers) {
   drivers.sort (function (driverOne, driverTwo) {
-    
+    return driverOne.revenue - driverTwo.revenue
   })
 }
